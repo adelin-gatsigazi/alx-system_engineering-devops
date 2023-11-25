977a754c79ed2584083e9213aaaf405a64de445c
adelin-gatsigazi <gatsigazi.adelin@gmail.com>
2023-11-25 04:42:36 +0200

    install flask

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,15 +1,5 @@
-# 1-install_a_package.pp
-
-class { 'python':
-  version => 'present',
-}
-
-package { 'python3-pip':
-  ensure => 'installed',
-}
-
-package { 'Flask':
+#Install flask / Version must be 2.1.0
+package { 'flask':
   ensure   => '2.1.0',
-  provider => 'pip',
-  require  => Package['python3-pip'],
-} 
+  provider => 'pip3'
+}
